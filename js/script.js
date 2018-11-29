5b8cd8bbeb99b5ceb8d7bb1474d3c97d35e21fcc
saver711 <saver711>
2018-11-29 18:18:05 +0200

    ____

--- js/script.js
+++ js/script.js
@@ -1,42 +1,41 @@
-/*global console, $, */
+/*global console, $, window*/
 /********************** Home **************************/
 /******** Nav ********/
 
 // 'use strict';
 // console.log('hi there');
+function scrollToElement(element, time) {
+    'use strict';
+    $('html, body').animate({
+        'scrollTop': $(element).offset().top + 1
+    }, time);
+}
+/**********************S Header **********************/
 
+/**********************E Header **********************/
 
-
+/*********************S When Window Load*************************/
 $(function () {
     'use strict';
+    /**********************S Header **********************/
+    $(window).on('scroll', function () {
+        if (window.scrollY >= $('.upper-nav__social').offset().top + 24) {
+            $('.lower-nav').addClass('whenScroll');
+        } else {
+            $('.lower-nav').removeClass('whenScroll');
+        }
+    });
+    /**********************E Header **********************/
+    
     /****************** S components ***************/
 
-    /*********** S Nav **************/
-
-     /*********** e Nav **************/
     
-    /********S Header ********/
 
-    /********E Header ********/
-    
-    
-    /******** S products ********/
-
-    /******** E products ********/
     
     
     /****************** E components ***************/
-    
-    
-    /******** S product-info ********/
 
-    /******** E product-info ********/
-    
-    
-    
-    
-    
 });
-
+/*********************E When Window Load*************************/
 
 
